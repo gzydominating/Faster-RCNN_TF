263f35a71dbb86a6e39d9f932405ec94de0ca8fa
gzydominating <gzy409338163@163.com>
2019-08-29 10:01:21 +0800

    Update network.py

--- lib/networks/network.py
+++ lib/networks/network.py
@@ -5,7 +5,7 @@ import roi_pooling_layer.roi_pooling_op_grad
 from rpn_msr.proposal_layer_tf import proposal_layer as proposal_layer_py
 from rpn_msr.anchor_target_layer_tf import anchor_target_layer as anchor_target_layer_py
 from rpn_msr.proposal_target_layer_tf import proposal_target_layer as proposal_target_layer_py
-
+from fast_rcnn.config import cfg
 
 
 DEFAULT_PADDING = 'SAME'
@@ -45,16 +45,16 @@ class Network(object):
         if data_path.endswith('.ckpt'):
             saver.restore(session, data_path)
         else:
-            data_dict = np.load(data_path).item()
+            data_dict = np.load(data_path,encoding="latin1").item()
             for key in data_dict:
                 with tf.variable_scope(key, reuse=True):
                     for subkey in data_dict[key]:
                         try:
                             var = tf.get_variable(subkey)
                             session.run(var.assign(data_dict[key][subkey]))
-                            print "assign pretrain model "+subkey+ " to "+key
+                            print( "assign pretrain model "+subkey+ " to "+key)
                         except ValueError:
-                            print "ignore "+key
+                            print( "ignore "+key)
                             if not ignore_missing:
 
                                 raise
@@ -63,12 +63,12 @@ class Network(object):
         assert len(args)!=0
         self.inputs = []
         for layer in args:
-            if isinstance(layer, basestring):
+            if isinstance(layer, str):
                 try:
                     layer = self.layers[layer]
-                    print layer
+                    print( layer)
                 except KeyError:
-                    print self.layers.keys()
+                    print( self.layers.keys())
                     raise KeyError('Unknown layer name fed: %s'%layer)
             self.inputs.append(layer)
         return self
@@ -77,7 +77,7 @@ class Network(object):
         try:
             layer = self.layers[layer]
         except KeyError:
-            print self.layers.keys()
+            print( self.layers.keys())
             raise KeyError('Unknown layer name fed: %s'%layer)
         return layer
 
@@ -85,8 +85,8 @@ class Network(object):
         id = sum(t.startswith(prefix) for t,_ in self.layers.items())+1
         return '%s_%d'%(prefix, id)
 
-    def make_var(self, name, shape, initializer=None, trainable=True):
-        return tf.get_variable(name, shape, initializer=initializer, trainable=trainable)
+    def make_var(self, name, shape, initializer=None, trainable=True, regularizer=None):
+        return tf.get_variable(name, shape, initializer=initializer, trainable=trainable, regularizer=regularizer)
 
     def validate_padding(self, padding):
         assert padding in ('SAME', 'VALID')
@@ -94,7 +94,7 @@ class Network(object):
     @layer
     def conv(self, input, k_h, k_w, c_o, s_h, s_w, name, relu=True, padding=DEFAULT_PADDING, group=1, trainable=True):
         self.validate_padding(padding)
-        c_i = input.get_shape()[-1]
+        c_i = int(input.get_shape()[-1])
         assert c_i%group==0
         assert c_o%group==0
         convolve = lambda i, k: tf.nn.conv2d(i, k, [1, s_h, s_w, 1], padding=padding)
@@ -102,7 +102,8 @@ class Network(object):
 
             init_weights = tf.truncated_normal_initializer(0.0, stddev=0.01)
             init_biases = tf.constant_initializer(0.0)
-            kernel = self.make_var('weights', [k_h, k_w, c_i/group, c_o], init_weights, trainable)
+            kernel = self.make_var('weights', [k_h, k_w, c_i/group, c_o], init_weights, trainable, \
+                                   regularizer=self.l2_regularizer(cfg.TRAIN.WEIGHT_DECAY))
             biases = self.make_var('biases', [c_o], init_biases, trainable)
 
             if group==1:
@@ -117,6 +118,32 @@ class Network(object):
                 return tf.nn.relu(bias, name=scope.name)
             return tf.nn.bias_add(conv, biases, name=scope.name)
 
+    @layer
+    def conv2(self, input, k_h, k_w, c_o, s_h, s_w, name, biased=True,relu=True, padding=DEFAULT_PADDING, trainable=True):
+        """ contribution by miraclebiu, and biased option"""
+        self.validate_padding(padding)
+        c_i = input.get_shape()[-1]
+        convolve = lambda i, k: tf.nn.conv2d(i, k, [1, s_h, s_w, 1], padding=padding)
+        with tf.variable_scope(name) as scope:
+
+            # init_weights = tf.truncated_normal_initializer(0.0, stddev=0.001)
+            init_weights = tf.contrib.layers.variance_scaling_initializer(factor=0.01, mode='FAN_AVG', uniform=False)
+            init_biases = tf.constant_initializer(0.0)
+            kernel = self.make_var('weights', [k_h, k_w, c_i, c_o], init_weights, trainable, \
+                                   regularizer=self.l2_regularizer(cfg.TRAIN.WEIGHT_DECAY))
+            if biased:
+                biases = self.make_var('biases', [c_o], init_biases, trainable)
+                conv = convolve(input, kernel)
+                if relu:
+                    bias = tf.nn.bias_add(conv, biases)
+                    return tf.nn.relu(bias)
+                return tf.nn.bias_add(conv, biases)
+            else:
+                conv = convolve(input, kernel)
+                if relu:
+                    return tf.nn.relu(conv)
+                return conv
+        
     @layer
     def relu(self, input, name):
         return tf.nn.relu(input, name=name)
@@ -148,7 +175,7 @@ class Network(object):
         if isinstance(input[1], tuple):
             input[1] = input[1][0]
 
-        print input
+        print( input)
         return roi_pool_op.roi_pool(input[0], input[1],
                                     pooled_height,
                                     pooled_width,
@@ -208,6 +235,17 @@ class Network(object):
              return tf.transpose(tf.reshape(tf.transpose(input,[0,3,1,2]),[input_shape[0],
                     int(d),tf.cast(tf.cast(input_shape[1],tf.float32)*(tf.cast(input_shape[3],tf.float32)/tf.cast(d,tf.float32)),tf.int32),input_shape[2]]),[0,2,3,1],name=name)
 
+            
+    @layer
+    def spatial_reshape_layer(self, input, d, name):
+        input_shape = tf.shape(input)
+        # transpose: (1, H, W, A x d) -> (1, H, WxA, d)
+        return tf.reshape(input,\
+                               [input_shape[0],\
+                                input_shape[1], \
+                                -1,\
+                                int(d)])
+            
     @layer
     def feature_extrapolating(self, input, scales_base, num_scale_base, num_per_octave, name):
         return feature_extrapolating_op.feature_extrapolating(input,
@@ -252,7 +290,8 @@ class Network(object):
                 init_weights = tf.truncated_normal_initializer(0.0, stddev=0.01)
                 init_biases = tf.constant_initializer(0.0)
 
-            weights = self.make_var('weights', [dim, num_out], init_weights, trainable)
+            weights = self.make_var('weights', [dim, num_out], init_weights, trainable, \
+								   regularizer=self.l2_regularizer(cfg.TRAIN.WEIGHT_DECAY))
             biases = self.make_var('biases', [num_out], init_biases, trainable)
 
             op = tf.nn.relu_layer if relu else tf.nn.xw_plus_b
@@ -268,5 +307,35 @@ class Network(object):
             return tf.nn.softmax(input,name=name)
 
     @layer
+    def spatial_softmax(self, input, name):
+        input_shape = tf.shape(input)
+        # d = input.get_shape()[-1]
+        return tf.reshape(tf.nn.softmax(tf.reshape(input, [-1, input_shape[3]])),
+                          [-1, input_shape[1], input_shape[2], input_shape[3]], name=name)
+        
+    @layer
+    def add(self,input,name):
+        """contribution by miraclebiu"""
+        return tf.add(input[0],input[1], name=name)
+        
+    @layer
+    def batch_normalization(self,input,name,relu=True, is_training=False):
+        """contribution by miraclebiu"""
+        if relu:
+            temp_layer=tf.contrib.layers.batch_norm(input,scale=True,center=True,is_training=is_training,scope=name)
+            return tf.nn.relu(temp_layer)
+        else:
+            return tf.contrib.layers.batch_norm(input,scale=True,center=True,is_training=is_training,scope=name)    
+        
+    @layer
     def dropout(self, input, keep_prob, name):
         return tf.nn.dropout(input, keep_prob, name=name)
+
+    def l2_regularizer(self, weight_decay=0.0005, scope=None):
+        def regularizer(tensor):
+            with tf.name_scope(scope, default_name='l2_regularizer', values=[tensor]):
+                l2_weight = tf.convert_to_tensor(weight_decay,
+                                       dtype=tensor.dtype.base_dtype,
+                                       name='weight_decay')
+                return tf.multiply(l2_weight, tf.nn.l2_loss(tensor), name='value')
+        return regularizer
